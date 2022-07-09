e702095b56b65122cf9a9a8c325ca40e62171ddf
turzoxpress1 <turzoxpress@gmail.com>
2022-07-09 23:37:49 +0600

    redesigning app

--- src/component/pages/ToDo.js
+++ src/component/pages/ToDo.js
@@ -7,9 +7,19 @@ import "react-dropdown/style.css";
 const ToDo = ({ todo, handleToggle, handleDelete, isLoggedIn }) => {
   // console.log("isLoggedIn value ToDo : " + isLoggedIn);
 
-  const handleClick = (id) => {
-    // e.preventDefault();
-    handleToggle(todo._id + "_" + id.value);
+  const my_id = todo._id;
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    let select = document.getElementById(my_id);
+    let option = select.options[select.selectedIndex].text;
+
+    let tempVar = option.toLowerCase();
+    if (tempVar === "todo") {
+      tempVar = "created";
+    }
+
+    handleToggle(my_id + "_" + tempVar);
   };
 
   const handleDeleteClick = (e) => {
@@ -34,50 +44,74 @@ const ToDo = ({ todo, handleToggle, handleDelete, isLoggedIn }) => {
   // };
 
   return (
-    <div className="center_div custom_div_center">
-      <p
-        id={todo._id + "_" + todo.status}
-        key={todo._id + todo.task_name}
-        name="todo"
-        value={todo.task_name}
-        // className={todo.status === "completed" ? "todo strike" : "todo"}
-        className="todo"
-      >
-        {todo.task_name}
-      </p>
+    <div>
+      <div>
+        <div className="card m-5">
+          <div className="card-body">
+            <div className="right_button_align">
+              <div className="drop_down_div m-2">
+                {/* <label for="input-select">Example Select</label> */}
+                <select
+                  className="form-control"
+                  id={my_id}
+                  options={options}
+                  value={defaultOption}
+                  onChange={handleClick}
+                >
+                  <option value={options[0]}>ToDo</option>
+                  <option value={options[1]}>Working</option>
+                  <option value={options[2]}>Completed</option>
+                </select>
+              </div>
+            </div>
 
-      {isLoggedIn ? (
-        <div>
-          <div>
-            <Dropdown
-              className="m-2"
-              options={options}
-              value={defaultOption}
-              placeholder="Select an option"
-              onChange={handleClick}
-            />
-          </div>
-          <div>
-            <button
-              className={"close_button"}
-              id={todo._id}
-              onClick={handleDeleteClick}
-            >
-              X
-            </button>
+            <h3 className="card-title card_title">{todo.task_name}</h3>
+            <p className="card-text card_title">{todo.task_description}</p>
+            <div className="right_button_align">
+              <a href="#" className="btn btn-danger">
+                Delete
+              </a>
+            </div>
           </div>
         </div>
-      ) : (
-        <div></div>
-      )}
+      </div>
 
-      {/* <div className="bg-dark p-2">
-        <CloseButton
-          id={todo._id}
-          className={"close_button"}
-          variant="red"
-          onClick={handleDeleteClick}
-        />
+      {/* <div className="center_div custom_div_center">
+        <p
+          id={todo._id + "_" + todo.status}
+          key={todo._id + todo.task_name}
+          name="todo"
+          value={todo.task_name}
+          // className={todo.status === "completed" ? "todo strike" : "todo"}
+          className="todo"
+        >
+          {todo.task_name}
+        </p>
+
+        {isLoggedIn ? (
+          <div>
+            <div>
+              <Dropdown
+                className="m-2"
+                options={options}
+                value={defaultOption}
+                placeholder="Select an option"
+                onChange={handleClick}
+              />
+            </div>
+            <div>
+              <button
+                className={"close_button"}
+                id={todo._id}
+                onClick={handleDeleteClick}
+              >
+                X
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
       </div> */}
     </div>
   );
